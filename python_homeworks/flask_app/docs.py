da90ab817dfa704be39b923e9af82a7352eb0a77
Hernan3scobar <hernanescobarsanchez@gmail.com>
2025-03-03 16:46:46 -0300

    Rename home_works_2 to flask_app

---
+++ python_homeworks/flask_app/docs.py
@@ -0,0 +1,19 @@
+from flask import jsonify, request  
+
+messages = [
+    {
+        "name": "John",
+        "message": "Hello, World!"
+    },
+    {
+        "name": "Jane",
+        "message": "Hello, John!"
+    },
+    {
+        "name": "John",
+        "message": "Hello, Jane!"
+    }
+    
+    
+    
+]
