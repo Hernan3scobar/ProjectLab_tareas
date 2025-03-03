38bb8d3581d310d883f15e45de3086c8879d6fbf
Hernan3scobar <hernanescobarsanchez@gmail.com>
2025-03-03 15:27:28 -0300

    Http and https finder

---
+++ bash_redex_exercises/regex_16.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+read -p "Enter a file path: " path
+
+[ -z "$path" ] && echo "Error: No file path provided." && exit 1
+[ ! -f "$path" ] && echo "Error: File '$path' does not exist." && exit 1
+
+grep -Eo "https?://[a-zA-Z0-9./?=_-]*" "$path"
+
